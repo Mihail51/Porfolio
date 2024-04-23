539866b78572930dbe3dbad5b8195dff71dc969f
Mihail51 <Mihail51@Gmail.com>
2024-04-23 17:08:53 +0300

    Set up the project build

---
+++ src/index.js
@@ -0,0 +1,83 @@
+import 'normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './styles/main.scss'
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+
+var swiper = new Swiper(".offerSwiper", {
+      slidesPerView: 3,
+      centeredSlides: false,
+      slidesPerGroupSkip: 1,
+      grabCursor: true,
+      keyboard: {
+        enabled: true,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+        },
+        576: {
+          slidesPerView: 2,
+          slidesPerGroup: 1,
+        },
+        1200: {
+          slidesPerView: 3,
+          slidesPerGroup: 1
+        }
+      },   
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    });
+var swiper = new Swiper(".trendsSwiper", {
+      slidesPerView: 3,
+      centeredSlides: false,
+      slidesPerGroupSkip: 1,
+      grabCursor: true,
+      keyboard: {
+        enabled: true,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+        },
+        576: {
+          slidesPerView: 2,
+          slidesPerGroup: 1,
+        },
+        1200: {
+          slidesPerView: 3,
+          slidesPerGroup: 1
+        }
+      },   
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    });
